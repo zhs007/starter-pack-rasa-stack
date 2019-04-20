53c0eebf823e565e5d04ddccfd46aee05f77ec7f
zhs007 <sssxueren@gmail.com>
2019-04-20 13:32:21 +0800

    - updated data

--- train.sh
+++ train.sh
@@ -3,5 +3,5 @@ docker build -t rasa-demo .
 mkdir models
 mkdir models/current
 
-docker run -v $PWD/models:/app/rasa-demo/models rasa-demo make train-nlu
-docker run -v $PWD/models:/app/rasa-demo/models rasa-demo make train-core
+docker run -v $PWD/models:/app/rasa-demo/models rasa-demo python -m rasa_nlu.train -c nlu_config.yml --data data/test001/nlu_data.md -o models --fixed_model_name nlu --project current --verbose
+docker run -v $PWD/models:/app/rasa-demo/models rasa-demo python -m rasa_core.train -d data/test001/domain.yml -s data/test001/stories.md -o models/current/dialogue -c policies.yml
